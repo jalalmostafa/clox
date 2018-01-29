544ac078487bf16156302c3bcbf03e2eee7d03ac
Jalal Mostafa <jalalmostafa22@gmail.com>
2018-01-30 00:25:12 +0200

    Except: vardiac except

--- include/except.h
+++ include/except.h
@@ -1,6 +1,6 @@
 
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
-void except(const char* message);
+void except(const char* format, ...);
 // void except_catch(void* ret, const char* message, void (*f)(void*));
 #endif
--- src/except.c
+++ src/except.c
@@ -1,15 +1,14 @@
+#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-void except(const char* message) {
-	if(message != NULL) {
-		perror(message);
-	}
-	exit(EXIT_FAILURE);
+void except(const char* format, ...)
+{
+    va_list list;
+    va_start(list, format);
+    vfprintf(stderr, format, list);
+    va_end(list);
+    // if (interactive) {
+    //     exit(EXIT_FAILURE);
+    // }
 }
-
-// void except_catch(void* ret, const char* message, void (*f)(void* result)) {
-// 	if(message != NULL) {
-// 		perror(message);
-// 	}
-// 	f(ret);
-// }
