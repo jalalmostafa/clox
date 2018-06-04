e0be8485114d4523ca4d491b82380fb101592039
Jalal Mostafa <jalalmostafa22@gmail.com>
2018-06-05 01:23:47 +0300

    Parser: accept parent expression/statement

--- src/parse.c
+++ src/parse.c
@@ -787,21 +787,21 @@ void* accept_expr(ExpressionVisitor visitor, Expr* expr)
 {
     switch (expr->type) {
     case LITERAL:
-        return visitor.visitLiteral(expr->expr);
+        return visitor.visitLiteral(expr);
     case UNARY:
-        return visitor.visitUnary(expr->expr);
+        return visitor.visitUnary(expr);
     case BINARY:
-        return visitor.visitBinary(expr->expr);
+        return visitor.visitBinary(expr);
     case GROUPING:
-        return visitor.visitGrouping(expr->expr);
+        return visitor.visitGrouping(expr);
     case VARIABLE:
-        return visitor.visitVariable(expr->expr);
+        return visitor.visitVariable(expr);
     case ASSIGNMENT:
-        return visitor.visitAssignment(expr->expr);
+        return visitor.visitAssignment(expr);
     case LOGICAL:
-        return visitor.visitLogical(expr->expr);
+        return visitor.visitLogical(expr);
     case CALL:
-        return visitor.visitCallable(expr->expr);
+        return visitor.visitCallable(expr);
     }
     return NULL;
 }
@@ -810,21 +810,21 @@ void* accept(StmtVisitor visitor, Stmt* stmt)
 {
     switch (stmt->type) {
     case STMT_PRINT:
-        return visitor.visitPrint(stmt->realStmt);
+        return visitor.visitPrint(stmt);
     case STMT_EXPR:
-        return visitor.visitExpression(stmt->realStmt);
+        return visitor.visitExpression(stmt);
     case STMT_VAR_DECLARATION:
-        return visitor.visitVarDeclaration(stmt->realStmt);
+        return visitor.visitVarDeclaration(stmt);
     case STMT_BLOCK:
-        return visitor.visitBlock(stmt->realStmt);
+        return visitor.visitBlock(stmt);
     case STMT_IF_ELSE:
-        return visitor.visitIfElse(stmt->realStmt);
+        return visitor.visitIfElse(stmt);
     case STMT_WHILE:
-        return visitor.visitWhile(stmt->realStmt);
+        return visitor.visitWhile(stmt);
     case STMT_FUN:
-        return visitor.visitFun(stmt->realStmt);
+        return visitor.visitFun(stmt);
     case STMT_RETURN:
-        return visitor.visitReturn(stmt->realStmt);
+        return visitor.visitReturn(stmt);
     }
     return NULL;
 }
